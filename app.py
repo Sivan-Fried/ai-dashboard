1b34ef01a27a397a6c89c8f2bd078fffc288d08a
Sivan-Fried <sivane@aman.co.il>
2026-04-17 10:11:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import streamlit as st
+import pandas as pd
+
+st.markdown("""
+<style>
+body {direction: RTL; text-align: right;}
+</style>
+""", unsafe_allow_html=True)
+
 st.title("הדשבורד שלי")
+
+# טעינת נתונים
+projects = pd.read_excel("projects.xlsx")
+
+# הצגה
+st.subheader("פרויקטים")
+st.dataframe(projects)
