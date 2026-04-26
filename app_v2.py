9c898c7c057efb84197423ca52f82f7eabc34bde
Sivan-Fried <sivane@aman.co.il>
2026-04-26 22:56:01 +0300

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -24,7 +24,7 @@ st.set_page_config(
 )
 
 st.markdown('<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,300,0,0" />', unsafe_allow_html=True)
-
+st.markdown('<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />', unsafe_allow_html=True)
 
 # טעינת פונטים
 st.markdown('<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" />', unsafe_allow_html=True)
