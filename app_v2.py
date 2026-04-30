09b0aea8bfb676b867a8f3c73b696dc49b6c8187
Sivan-Fried <sivane@aman.co.il>
2026-04-30 12:38:04 +0300

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -1052,7 +1052,7 @@ else:
                 }
                 div[data-testid="stVerticalBlock"] > div:has(.fathom-row-ui) { gap: 0rem !important; }
                 div.element-container:has(.fathom-row-ui) + div.element-container {
-                    margin-top: -45px !important; margin-bottom: 2px !important;
+                    margin-top: -45px !important; margin-bottom: 1px !important;
                 }
                 div.element-container:has(.fathom-row-ui) + div.element-container div[data-testid="stButton"] button {
                     background: transparent !important; border: 1px solid transparent !important;
