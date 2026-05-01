a1b16e6040ed9add0b055affda5be895f9f1def0
Sivan-Fried <sivane@aman.co.il>
2026-05-01 09:37:53 +0300

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -732,7 +732,7 @@ else:
         </div>
         ''', unsafe_allow_html=True)
     
-    st.markdown("<br>", unsafe_allow_html=True)
+    #st.markdown("<br>", unsafe_allow_html=True)
 
     #הגדרת עמודה ימנית - לא למחוק
     col_right, col_left = st.columns([1, 1])
