bd28c42eda3848181c97a9aefad95873868427f8
Sivan-Fried <sivane@aman.co.il>
2026-05-03 11:06:51 +0300

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -648,7 +648,7 @@ if "collapsed" in st.query_params:
     st.session_state.sidebar_collapsed = st.query_params["collapsed"] == "true"
 
 if st.session_state.sidebar_collapsed:
-    sidebar_col, main_col = st.columns([0.03, 0.97])
+    sidebar_col, main_col = st.columns([0.05, 0.95])
 else:
     sidebar_col, main_col = st.columns([0.13, 0.87])
 
