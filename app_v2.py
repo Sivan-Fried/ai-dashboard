f4568fe99ae2e288c4f29bbfca20cd8d1bec286f
Sivan-Fried <sivane@aman.co.il>
2026-05-04 11:42:59 +0300

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -77,7 +77,7 @@ st.markdown("""
         padding: 25px 60px 10px 60px !important;
         
         /* צמצום הרווח מה-KPIs (ערך שלילי כאן מקרב את ה-KPIs לציטוט) */
-        margin-bottom: -16px !important; 
+        margin-bottom: 10px !important; 
         
         z-index: 1 !important;
     }
