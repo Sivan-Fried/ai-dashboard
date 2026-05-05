0e2a2771a8be44475e5428e81f67148d47073551
Sivan-Fried <sivane@aman.co.il>
2026-05-05 14:59:30 +0300

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -493,7 +493,8 @@ def render_topbar_with_bell(img_b64, w_text, w_city, greeting, today_reminders):
 
   document.getElementById('homeBtn').addEventListener('click', function() {{
     var url = window.parent.location.href.split('?')[0];
-    window.parent.location.href = url;
+    window.parent.history.pushState({{}}, '', url);
+    window.parent.location.replace(url);
   }});
 </script>
 </body>
