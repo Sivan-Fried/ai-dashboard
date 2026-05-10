9c4c6ee290e05075116aee4d62ecefa273d77265
Sivan-Fried <sivane@aman.co.il>
2026-05-10 20:36:18 +0300

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -753,7 +753,7 @@ if "ai_response"     not in st.session_state: st.session_state.ai_response     =
 if "adding_reminder" not in st.session_state: st.session_state.adding_reminder = False
 
 params = st.query_params
-if "proj" in params and st.session_state.current_page != "resources":
+if "proj" in params and st.session_state.current_page not in ["resources", "risks", "meetings"]:
     st.session_state.selected_project = params["proj"]
     st.session_state.current_page = "project"
 if "page" in params and params["page"] == "resources":
