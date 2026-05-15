20feaf2afce78043b4385a37d537f82da5ed2a0e
Sivan-Fried <sivane@aman.co.il>
2026-05-15 18:28:49 +0300

    Update risks_module.py

--- risks_module.py
+++ risks_module.py
@@ -260,7 +260,7 @@ def show_risks_page(project_name=None):
     """, unsafe_allow_html=True)
 
     #רווח מתחת לאזור KPI
-    #st.markdown("<div style='margin-bottom:0.5rem;'></div>", unsafe_allow_html=True)
+    st.markdown("<div style='margin-bottom:-0.5rem;'></div>", unsafe_allow_html=True)
 
     # ── טבלת סיכונים ─────────────────────────────────────────
     st.markdown("### פירוט סיכונים", unsafe_allow_html=True)
